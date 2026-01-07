54b1bfcf04c24708f6e7846fe89749ce2d06df50
jorge <jorgesuarezbaez@gmail.com>
2026-01-07 18:58:57 +0000

    HistogramBuilder class modified

--- src/main/java/software/ulpgc/kata4/architecture/viewmodel/HistogramBuilder.java
+++ src/main/java/software/ulpgc/kata4/architecture/viewmodel/HistogramBuilder.java
@@ -2,25 +2,50 @@ package software.ulpgc.kata4.architecture.viewmodel;
 
 import software.ulpgc.kata4.architecture.model.Movie;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class HistogramBuilder {
-    private final Function<Movie, Integer> binarize;
+    private final Map<String, String> labels;
+    private final Stream<Movie> movies;
 
-    public HistogramBuilder(Function<Movie, Integer> binarize) {
-        this.binarize = binarize;
+    public static HistogramBuilder with(Stream<Movie> movies) {
+        if (movies == null) throw new IllegalArgumentException("No movies found");
+        return new HistogramBuilder(movies);
     }
 
-    public Histogram buildWith(List<Movie> movieList) {
-        Histogram histogram = new Histogram();
-        for (Movie movie: movieList) {
-            histogram.add(binOf(movie));
-        }
+    public HistogramBuilder(Stream<Movie> movies) {
+        this.labels = new HashMap<>();
+        this.movies = movies
+                .filter(m->m.year() >= 1900)
+                .filter(m->m.year() <= 2025);
+    }
+
+    public HistogramBuilder tittle(String label) {
+        labels.put("tittle", label);
+        return this;
+    }
+
+    public HistogramBuilder x(String label) {
+        labels.put("x", label);
+        return this;
+    }
+
+    public HistogramBuilder y(String label) {
+        labels.put("y", label);
+        return this;
+    }
+
+    public HistogramBuilder legend(String label) {
+        labels.put("legend", label);
+        return this;
+    }
+
+    public Histogram use(Function<Movie, Integer> binarize) {
+        Histogram histogram = new Histogram(labels);
+        movies.map(binarize).forEach(histogram::add);
         return histogram;
-    }
-
-    private int binOf(Movie movie) {
-        return binarize.apply(movie);
     }
 }
